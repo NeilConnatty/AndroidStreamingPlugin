e75929c17cfd483651b4a7b1752beb5646cf1310
neilconnatty <neil.connatty@gmail.com>
2016-10-11 14:07:09 -0700

    removed title bar from theme

--- app/src/main/java/com/bcch/neilconnatty/streamingplugin/activities/MainActivity.java
+++ app/src/main/java/com/bcch/neilconnatty/streamingplugin/activities/MainActivity.java
@@ -1,23 +1,35 @@
 package com.bcch.neilconnatty.streamingplugin.activities;
 
 import android.os.Bundle;
+import android.view.Window;
 
+import com.bcch.neilconnatty.libstreamingplugin.StreamingPlugin;
 import com.bcch.neilconnatty.libstreamingplugin.activites.BaseActivity;
 import com.bcch.neilconnatty.streamingplugin.R;
+import com.crashlytics.android.Crashlytics;
 import com.quickblox.videochat.webrtc.view.QBRTCSurfaceView;
 
-public class MainActivity extends BaseActivity {
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends BaseActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with (this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        setViewReferences();
+
+        StreamingPlugin plugin = new StreamingPlugin (this, false);
+        plugin.StartStreamingPlugin ();
     }
 
     @Override
     protected void setViewReferences()
     {
-        opponentView = (QBRTCSurfaceView) findViewById(R.id.opponentView);
+        opponentView = null;
         localView = (QBRTCSurfaceView) findViewById(R.id.localView);
     }
 }
