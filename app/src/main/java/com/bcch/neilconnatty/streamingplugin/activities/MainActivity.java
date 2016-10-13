2d0f58ddb21ca815011103149918bd74f9a4624b
neilconnatty <neil.connatty@gmail.com>
2016-10-13 13:57:10 -0700

    added listener for keypresses to mainActivity

--- app/src/main/java/com/bcch/neilconnatty/streamingplugin/activities/MainActivity.java
+++ app/src/main/java/com/bcch/neilconnatty/streamingplugin/activities/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bcch.neilconnatty.libstreamingplugin.StreamingPlugin;
@@ -27,6 +30,7 @@ public class MainActivity extends BaseActivity
 
     private ImagePagerAdapter mAdapter;
     private ViewPager mPager;
+    private int _currentPosition;
 
 
     /** A static dataset to back the ViewPager adapter */
@@ -36,16 +40,14 @@ public class MainActivity extends BaseActivity
 
     /** Constructor */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         Fabric.with (this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         setViewReferences();
-
-        mAdapter = new ImagePagerAdapter(getSupportFragmentManager(), imageResIds.length);
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPager.setAdapter(mAdapter);
+        initImageAdapter();
 
         StreamingPlugin plugin = new StreamingPlugin (this, false);
         plugin.StartStreamingPlugin ();
@@ -54,41 +56,93 @@ public class MainActivity extends BaseActivity
 
     /*********** Public Methods **********/
 
-    public void loadBitmap(int resId, ImageView imageView) {
+    public void loadBitmap (int resId, ImageView imageView)
+    {
         imageView.setImageResource(R.drawable.image_placeholder);
         BitmapWorkerTask task = new BitmapWorkerTask(imageView);
         task.execute(resId);
+    }
+
+    @Override
+    public boolean onKeyDown (int keyCode, KeyEvent event)
+    {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+                if (mPager.getVisibility() == View.VISIBLE) mPager.setVisibility(View.INVISIBLE);
+                else mPager.setVisibility(View.VISIBLE);
+                return true;
+
+            case KeyEvent.KEYCODE_MENU:
+                Log.d(TAG, "KEYCODE_MENU");
+                return true;
+
+            case KeyEvent.KEYCODE_BACK:
+                Log.d(TAG, "KEYCODE_BACK");
+                return true;
+        }
+
+        return false;
     }
 
 
     /*********** Private/Protected Methods **********/
 
     @Override
-    protected void setViewReferences()
+    protected void setViewReferences ()
     {
         opponentView = null;
         localView = (QBRTCSurfaceView) findViewById(R.id.localView);
     }
 
+    private void initImageAdapter ()
+    {
+        mAdapter = new ImagePagerAdapter(getSupportFragmentManager(), imageResIds.length);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+        mPager.setVisibility(View.VISIBLE);
+        _currentPosition = 0;
+        createPagerListener(mPager);
+    }
+
+    private void createPagerListener (ViewPager pager)
+    {
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener () {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                _currentPosition = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
 
     /*********** Local Classes **********/
 
     /** ImagePagerAdapter */
-    public static class ImagePagerAdapter extends FragmentStatePagerAdapter {
+    public static class ImagePagerAdapter extends FragmentStatePagerAdapter
+    {
         private final int mSize;
 
-        public ImagePagerAdapter(FragmentManager fm, int size) {
+        ImagePagerAdapter (FragmentManager fm, int size) {
             super(fm);
             mSize = size;
         }
 
         @Override
-        public int getCount() {
+        public int getCount () {
             return mSize;
         }
 
         @Override
-        public Fragment getItem(int position) {
+        public Fragment getItem (int position) {
             return ImageDetailFragment.newInstance(position);
         }
     }
@@ -99,21 +153,21 @@ public class MainActivity extends BaseActivity
         private final WeakReference<ImageView> imageViewReference;
         private int data = 0;
 
-        public BitmapWorkerTask(ImageView imageView) {
+        BitmapWorkerTask (ImageView imageView) {
             // Use a WeakReference to ensure the ImageView can be garbage collected
             imageViewReference = new WeakReference<ImageView>(imageView);
         }
 
         // Decode image in background.
         @Override
-        protected Bitmap doInBackground(Integer... params) {
+        protected Bitmap doInBackground (Integer... params) {
             data = params[0];
             return BitmapDecoder.decodeSampledBitmapFromResource (getResources(), data, 100, 100);
         }
 
         // Once complete, see if ImageView is still around and set bitmap.
         @Override
-        protected void onPostExecute(Bitmap bitmap) {
+        protected void onPostExecute (Bitmap bitmap) {
             if (imageViewReference != null && bitmap != null) {
                 final ImageView imageView = imageViewReference.get();
                 if (imageView != null) {
--- app/src/main/java/com/bcch/neilconnatty/streamingplugin/imageViewer/BitmapDecoder.java
+++ app/src/main/java/com/bcch/neilconnatty/streamingplugin/imageViewer/BitmapDecoder.java
@@ -10,8 +10,28 @@ import android.graphics.BitmapFactory;
 
 public class BitmapDecoder
 {
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    public static Bitmap decodeSampledBitmapFromResource
+              (Resources res, int resId, int reqWidth, int reqHeight)
+    {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+
+        /* This code for scaling down large image, however seems to scale down too much...
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        */
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    private static int calculateInSampleSize
+            (BitmapFactory.Options options, int reqWidth, int reqHeight)
+    {
         // Raw height and width of image
         final int height = options.outHeight;
         final int width = options.outWidth;
@@ -31,21 +51,5 @@ public class BitmapDecoder
         }
 
         return inSampleSize;
-    }
-
-    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
     }
 }
