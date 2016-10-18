d428137ad13e19121f69e7acc59f429d68db80e6
neilconnatty <neil.connatty@gmail.com>
2016-10-18 13:48:48 -0700

    deleted redundant code

--- libstreamingplugin/src/main/java/com/bcch/neilconnatty/libstreamingplugin/utils/UserLoginHelper.java
+++ libstreamingplugin/src/main/java/com/bcch/neilconnatty/libstreamingplugin/utils/UserLoginHelper.java
@@ -7,83 +7,30 @@ import com.quickblox.auth.QBAuth;
 import com.quickblox.auth.model.QBSession;
 import com.quickblox.chat.QBChatService;
 import com.quickblox.core.QBEntityCallback;
-import com.quickblox.core.QBEntityCallbackImpl;
 import com.quickblox.core.exception.QBResponseException;
 import com.quickblox.core.helper.StringifyArrayList;
 import com.quickblox.users.model.QBUser;
 
 /**
  * Created by neilconnatty on 2016-09-29.
- * Extended from Quickblox sample projects at https://github.com/QuickBlox/quickblox-android-sdk/
  */
 
 public class UserLoginHelper
 {
     private final String TAG = UserLoginHelper.class.getSimpleName();
 
-    private QBResRequestExecutor requestExecutor;
-
 
     /************ Public Methods *********/
-
-    /** Constructor */
-    public UserLoginHelper ()
-    {
-        requestExecutor = new QBResRequestExecutor ();
-    }
 
     public QBUser createUserWithDefaultData ()
     {
         return createUser(Consts.DEFAULT_USER_LOGIN, Consts.DEFAULT_ROOM_NAME);
     }
 
-    /*
-    public void startSignUpNewUser (final QBUser newUser, final QBEntityCallback<QBUser> callback)
-    {
-        requestExecutor.signUpNewUser(newUser, new QBEntityCallback<QBUser>() {
-                    @Override
-                    public void onSuccess(QBUser result, Bundle params) {
-                        loginUser(newUser, new QBEntityCallback<QBUser>() {
-                            @Override
-                            public void onSuccess(QBUser qbUser, Bundle bundle) {
-                                callback.onSuccess(newUser, bundle);
-                            }
-
-                            @Override
-                            public void onError(QBResponseException e) {
-                                throw new RuntimeException ("error logging in user");
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onError(QBResponseException e) {
-                        if (e.getHttpStatusCode() == Consts.ERR_LOGIN_ALREADY_TAKEN_HTTP_STATUS) {
-                            signInCreatedUser(newUser, true, new QBEntityCallback<QBUser>() {
-                                @Override
-                                public void onSuccess(QBUser qbUser, Bundle bundle) {
-                                    callback.onSuccess(newUser, bundle);
-                                }
-
-                                @Override
-                                public void onError(QBResponseException e) {
-                                    throw new RuntimeException("error signing up created user");
-                                }
-                            });
-                        } else {
-                            throw new RuntimeException ("error signing up new user");
-                        }
-                    }
-                }
-        );
-    }
-    */
     public void startSignUpNewUser (final QBUser qbUser, final QBEntityCallback<QBUser> callback)
     {
         createSession(qbUser, callback);
     }
-
-
 
 
     /************ Private Methods ************/
@@ -135,57 +82,4 @@ public class UserLoginHelper
             }
         });
     }
-    /*
-    private void signInCreatedUser (final QBUser user, final boolean deleteCurrentUser, final QBEntityCallback<QBUser> callback) {
-        requestExecutor.signInUser(user, new QBEntityCallbackImpl<QBUser>() {
-            @Override
-            public void onSuccess(QBUser result, Bundle params) {
-                if (deleteCurrentUser) {
-                    removeAllUserData(result, new QBEntityCallback<QBUser>() {
-                        @Override
-                        public void onSuccess(QBUser qbUser, Bundle bundle) {
-                            callback.onSuccess(user, bundle);
-                        }
-
-                        @Override
-                        public void onError(QBResponseException e) {
-                            callback.onError(e);
-                        }
-                    });
-                } else {
-                    // TODO
-                }
-            }
-
-            @Override
-            public void onError(QBResponseException responseException) {
-                throw new RuntimeException ("error signing in created user");
-            }
-        });
-    }
-
-    private void removeAllUserData (final QBUser user, final QBEntityCallback<QBUser> callback) {
-        requestExecutor.deleteCurrentUser(user.getId(), new QBEntityCallback<Void>() {
-            @Override
-            public void onSuccess(Void aVoid, Bundle bundle) {
-                startSignUpNewUser(createUserWithDefaultData(), new QBEntityCallback<QBUser>() {
-                    @Override
-                    public void onSuccess(QBUser qbUser, Bundle bundle) {
-                        callback.onSuccess(user, bundle);
-                    }
-
-                    @Override
-                    public void onError(QBResponseException e) {
-                        callback.onError(e);
-                    }
-                });
-            }
-
-            @Override
-            public void onError(QBResponseException e) {
-                throw new RuntimeException ("error removing user data");
-            }
-        });
-    }
-    */
 }
